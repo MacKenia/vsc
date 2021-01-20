649bb6ad261547b1e2db42028ae8de4f7fd49d37
MacKenia <maikenqi@outlook.com>
2021-01-20 19:13:22 +0800

    20

--- C++/ji_xun_dong/19D.c
+++ C++/ji_xun_dong/19D.c
@@ -8,18 +8,44 @@ struct word
 
 int main()
 {
-
+    void front(struct word *p, struct word *ah);
+    void behind(struct word *p, struct word *l);
+    void (*en)(struct word*, struct word*);
+    struct word *p, *q, head;
+    char t;
+    p = &head;
+    while (~scanf("%d", &t))
+    {
+        q = (struct word *) malloc(sizeof(struct word));
+        en = behind;
+        if (t == '[')
+        {
+            en = front;
+            p = &head;
+        }
+        else if (t == ']')
+        {
+            en = behind;
+            p = q;
+        }
+        else
+        {
+            q -> w = t;
+            void (*en)(p, q);
+        }
+    }
+    
     return 0;
 }
 
-void front(struct word *p,struct word *h)
+void front(struct word *p,struct word *ah)
 {
-    p->next = h;
-    h = p;
+    ah->next = p->next;
+    p->next = ah;
 }
 
 void behind(struct word *p,struct word *l)
 {
-    l->next = p;
-    p->next = NULL;
+    p->next = l;
+    l->next = NULL;
 }

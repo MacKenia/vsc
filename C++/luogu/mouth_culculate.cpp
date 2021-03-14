85e2642b2215b05720c1ad92b3a5800a3a452bea
MacKenia <maikenqi@outlook.com>
2021-03-14 17:08:37 +0800

    3.14x

--- C++/luogu/mouth_culculate.cpp
+++ C++/luogu/mouth_culculate.cpp
@@ -4,6 +4,32 @@ using namespace std;
 
 int main()
 {
-    
+    int a(int a, int b);
+    int b(int a, int b);
+    int c(int a, int b);
+    int (*p)(int, int);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        char num1, num2, num3;
+        
+        cout << (*p)(num2, num3) << endl;
+    }
     return 0;
+}
+
+int a(int a, int b)
+{
+    return a+b;
+}
+
+int b(int a, int b)
+{
+    return a-b;
+}
+
+int c(int a, int b)
+{
+    return a*b;
 }

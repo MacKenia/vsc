1d6fe70e58c2e74d0449dfb10a14c569bfea25f9
MacKenia <maikenqi@outlook.com>
2021-03-24 18:32:37 +0800

    3.244

--- C++/learn/2021cpp/72_9.cpp
+++ C++/learn/2021cpp/72_9.cpp
@@ -2,7 +2,8 @@
 #include <algorithm>
 using namespace std;
 
-struct stu{
+struct stu
+{
     char name[21];
     int age;
     char address[51];
@@ -12,7 +13,7 @@ struct stu{
 //vector 容器
 vector<stu> stud;
 //添加学生数据函数
-int addRcord()
+int addRecord()
 {
     cout << "请输入学生姓名：";
     cin >> example.name;
@@ -28,12 +29,14 @@ int addRcord()
     {
         cout << "请重新输入输入学生年龄；";
         cin >> example.age;
+        cin.ignore();
     }
     stud.push_back(example);
     return 1;
 }
-
-int show(stu stut){ 
+//for_each函数
+int show(stu stut)
+{ 
     cout << "姓名: " << stut.name << endl
     << "年龄: " << stut.age << endl
     << "电话: " << stut.phone << endl
@@ -41,10 +44,38 @@ int show(stu stut){
     << "邮件: " << stut.Email << endl << endl;
     return 1;
 }
-
-int showRcord()
+//显示记录
+int showRecord()
 {
     for_each(stud.begin(),stud.end(),show);
+    return 1;
+}
+//修改学生记录
+int modifyRecord()
+{
+    
+    return 1;
+}
+//查找记录
+int seekRecord()
+{
+    return 1;
+}
+//删除学生记录
+int eraseRecord()
+{
+    vector<stu>::iterator e = stud.begin();
+    int no = 0;
+    cout << "请输入需要删除的学生序号(输入0删除最后一个): ";
+    cin >> no;
+    cin.ignore(100,'\n');
+    if(!no) stud.pop_back();
+    else
+    {
+        no--;
+        while(no--) e++;
+        e = stud.erase(e);
+    }
     return 1;
 }
 
@@ -57,6 +88,7 @@ int main()
     if(file.fail()) cout << "打开失败!" << endl;
     else cout << "正在载入dat文件" << endl;
     file.clear();
+    file.flush();
     file.seekg(0L,ios::beg);
     while(!file.eof())
     {
@@ -64,11 +96,11 @@ int main()
         stud.push_back(tmp);
         cout << "正在载入第" << ++count << "条数据" << endl;
     }
-    cout << "一共载入" << count << "条数据";
+    cout << "一共载入 " << count << " 条数据";
     file.close();
     while(true)
     {
-        cout << endl << endl 
+        cout << endl
         << "*********** 菜单 ************" << endl
         << "(1)向文件中增加记录" << endl 
         << "(2)显示文件中的所有记录" << endl 
@@ -78,24 +110,24 @@ int main()
         << "(6)退出" << endl
         << "****************************" << endl
         << endl << "请输入数字: " ;
-        cin >> choice;  
+        cin >> choice;
+        cin.ignore(); 
         switch (choice) {
-            case 1: addRcord();
+            case 1: addRecord();
                     break;     
-            case 2: showRcord();
+            case 2: showRecord();
                     break;
-            case 3: 
+            case 3: modifyRecord();
                     break;
-            case 4: 
+            case 4: seekRecord();
                     break;
-            case 5: 
+            case 5: eraseRecord();
                     break;
-            case 6: 
-                    flag = true;
+            case 6: flag = true;
+                    break;
     }
     if(flag) break;
     cout << endl << "回车以继续";
-    cin.ignore();
     cin.get();
     }
     file.open("a.dat",ios::binary|ios::out);

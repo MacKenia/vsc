e17f7c7d9e790cc9195dffca81b451a127a6f5ab
MacKenia <maikenqi@outlook.com>
2021-04-11 16:34:13 +0800

    Update main.cpp

--- C++/learn/2021cpp/114_1/main.cpp
+++ C++/learn/2021cpp/114_1/main.cpp
@@ -13,22 +13,34 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     Date i;
     int choice;
-    cin >> choice;
-    cout << "1.输入日期" << endl
-    << "2.以数字方式显示日期" << endl
-    << "3.以月在前显示" << endl
-    << "4.以日在前显示" << endl;
-    switch (choice) {
-        case 1:
-            <#statements#>
-            break;
-            
-        default:
-            break;
+    bool f = true;
+    while(f)
+    {
+        cout << "1.输入日期" << endl
+        << "2.以数字方式显示日期" << endl
+        << "3.以月在前显示" << endl
+        << "4.以日在前显示" << endl
+        << "5.退出" << endl;
+        cin >> choice;
+        switch (choice)
+        {
+            case 1:
+                i.setDate();
+                break;
+            case 2:
+                i.shownum();
+                break;
+            case 3:
+                i.showmonth();
+                break;
+            case 4:
+                i.showday();
+                break;
+            case 5:
+                f = false;
+        }
+        cout << endl;
+
     }
-    i.setDate();
-    i.showmonth();
-    i.showday();
-    i.shownum();
     return 0;
 }

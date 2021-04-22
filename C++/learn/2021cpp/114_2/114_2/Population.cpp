63e1aa7dfaa697e40888b64fcccaabbb57fb023a
MacKenia <maikenqi@outlook.com>
2021-04-22 19:37:24 +0800

    114_2

--- C++/learn/2021cpp/114_2/114_2/Population.cpp
+++ C++/learn/2021cpp/114_2/114_2/Population.cpp
@@ -23,8 +23,21 @@ Population::Population()
 {
     cout << "请输入当前的人数：";
     cin >> people;
+    while(people <= 1)
+    {
+        cout << "输入人数小于等于1，请重新输入：";
+        cin >> people;
+    }
     cout << "请输入出生的人数：";
     cin >> bp;
+    while (bp<0) {
+        cout << "输入人数小于0，请重新输入：";
+        cin >> bp;
+    }
     cout << "请输入死亡的人数：";
     cin >> dp;
+    while (dp < 0) {
+        cout << "输入人数小于0，请重新输入：";
+        cin >> dp;
+    }
 }

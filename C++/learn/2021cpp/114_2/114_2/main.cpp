c8f5d11402e59776e6ed9a1370aab763103343a3
MacKenia <maikenqi@outlook.com>
2021-04-22 19:30:02 +0800

    114_2

---
+++ C++/learn/2021cpp/114_2/114_2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  114_2
+//
+//  Created by MacKenia on 2021/4/22.
+//
+
+#include <iostream>
+#include "Population.h"
+using namespace std;
+
+int main() {
+    // insert code here...
+    Population thisyear;
+    cout << thisyear.bornrate() << endl;
+    cout << thisyear.deathrate() << endl;
+    return 0;
+}

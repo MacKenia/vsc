6b21283bf086b59d826fc9b06293ba5407def11f
MacKenia <maikenqi@outlook.com>
2021-04-22 20:27:36 +0800

    114_4

---
+++ C++/learn/2021cpp/114_4/114_4/Payroll.cpp
@@ -0,0 +1,16 @@
+//
+//  Payroll.cpp
+//  114_4
+//
+//  Created by MacKenia on 2021/4/22.
+//
+
+#include "Payroll.h"
+#include <iostream>
+using namespace std;
+
+payroll::payroll()
+{
+    cout << "";
+    cin >> 
+}
---
+++ C++/learn/2021cpp/114_4/114_4/Payroll.h
@@ -0,0 +1,22 @@
+//
+//  Payroll.hpp
+//  114_4
+//
+//  Created by MacKenia on 2021/4/22.
+//
+
+#ifndef Payroll_h
+#define Payroll_h
+class payroll
+{
+private:
+    float hsalary;
+    float moneyph;
+    float wtime;
+    float wwage;
+public:
+    payroll();
+    float pay();
+};
+
+#endif /* Payroll_h */

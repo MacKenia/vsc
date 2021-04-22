eb4d50243dfb8dc7372a4bd73a4452dd016d31b7
MacKenia <maikenqi@outlook.com>
2021-04-22 20:27:36 +0800

    114_4

---
+++ C++/learn/2021cpp/114_4/114_4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  114_4
+//
+//  Created by MacKenia on 2021/4/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

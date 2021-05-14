e593568c31b882254cb58267df73616399eed6a5
MacKenia <maikenqi@outlook.com>
2021-05-14 17:33:31 +0800

    new

---
+++ C++/learn/2021cpp/165_2/165_2/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  165_2
+//
+//  Created by MacKenia on 2021/5/14.
+//
+
+#include <iostream>
+#include "numday.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    numday a;
+    a.a();
+    return 0;
+}

cd181e42e1aa7868ab03a45a3aa835cd7bdd9f09
MacKenia <maikenqi@outlook.com>
2021-05-16 16:56:41 +0800

    165_1

--- C++/learn/2021cpp/165_1/165_1/main.cpp
+++ C++/learn/2021cpp/165_1/165_1/main.cpp
@@ -4,12 +4,17 @@
 //
 //  Created by MacKenia on 2021/5/14.
 //
-
 #include <iostream>
+#include "numday.h"
 using namespace std;
 
 int main() {
     // insert code here...
-    
+    numday a, b;
+    cin >> b;
+    a++;
+    b--;
+//    cin >> a >> b;
+    cout << a << b;
     return 0;
 }
--- C++/learn/2021cpp/165_1/165_1/numday.h
+++ C++/learn/2021cpp/165_1/165_1/numday.h
@@ -1,9 +1,19 @@
+#include <iostream>
 #ifndef numday_h
 #define numday_h
 class numday
 {
 private:
-     
+    float hour;
+    float day;
 public:
+    numday();
+    void converse();
+    numday& operator+(numday&);
+    numday& operator-(numday&);
+    numday& operator++(int);
+    numday& operator--(int);
+    friend std::ostream& operator<<(std::ostream&, numday&);
+    friend std::istream& operator>>(std::istream&, numday&);
 };
 #endif

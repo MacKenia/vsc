d7b582f40407a5b816ef5f5b0244bad72a3e4f1c
MacKenia <maikenqi@outlook.com>
2021-05-16 16:56:41 +0800

    165_1

---
+++ C++/learn/2021cpp/165_1/165_1/numday.cpp
@@ -0,0 +1,65 @@
+#include "numday.h"
+#include <iostream>
+using namespace std;
+
+void numday::converse()
+{
+    if(day > 0 && hour < 0)
+    {
+        day--;
+        hour+=8;
+    }
+    day += hour/8;
+    hour = 0;
+}
+
+numday::numday()
+{
+    day = 0;
+    hour = 0;
+    cout << "请输入工作小时：";
+    cin >> hour;
+    converse();
+}
+
+numday& numday::operator+(numday &a)
+{
+    day += a.day;
+    hour += a.hour;
+    converse();
+    return *this;
+}
+
+numday& numday::operator-(numday &a)
+{
+    day -= a.day;
+    return *this;
+}
+
+numday& numday::operator++(int)
+{
+    hour += 1;
+    converse();
+    return *this;
+}
+
+numday& numday::operator--(int)
+{
+    hour -= 1;
+    converse();
+    return *this;
+}
+
+ostream& operator<<(ostream &strm, numday &a)
+{
+    cout << a.day << "天 " << a.hour << "小时" << endl;
+    return strm;
+}
+
+istream& operator>>(istream &strm, numday &a)
+{
+    cout << "输入天数：";
+    cin >> a.day;
+    return strm;
+}
+

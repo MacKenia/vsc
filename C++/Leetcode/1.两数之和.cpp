50ac2873ab3943ef42ea0d9714c1589a5fb2ea5d
MacKenia <maikenqi@outlook.com>
2021-05-16 11:39:27 +0800

    1

---
+++ C++/Leetcode/1.两数之和.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
+
+// @lc code=end
+
---
+++ C++/learn/2021cpp/165_1/165_1/numday.h
@@ -0,0 +1,9 @@
+#ifndef numday_h
+#define numday_h
+class numday
+{
+private:
+     
+public:
+};
+#endif

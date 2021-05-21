823429d6cf4936d8e7a4b12b5bfb230171a68b14
MacKenia <maikenqi@outlook.com>
2021-05-21 12:08:54 +0800

    up

---
+++ C++/learn/2021cpp/196_7/196_7/basicshape.cpp
@@ -0,0 +1,19 @@
+//
+//  basicshape.cpp
+//  196_7
+//
+//  Created by MacKenia on 2021/5/21.
+//
+
+#include <iostream>
+#include "basicshape.h"
+using namespace std;
+
+// circle
+
+circle::circle()
+{
+    
+}
+
+// rectangle

3e4ba062c0240946a7c7600864cb3011b98b9ff8
MacKenia <maikenqi@outlook.com>
2021-05-23 14:21:04 +0800

    up

--- C++/learn/2021cpp/196_7/196_7/basicshape.cpp
+++ C++/learn/2021cpp/196_7/196_7/basicshape.cpp
@@ -13,7 +13,22 @@ using namespace std;
 
 circle::circle()
 {
-    
+    cout << "请输入X坐标：";
+    cin >> centerx;
+    cout << "请输入Y坐标：";
+    cin >> centery;
+    cout << "请输入半径：";
+    cin >> radius;
+    calcarea();
 }
 
 // rectangle
+
+rectangle::rectangle()
+{
+    cout << "请输入宽度：";
+    cin >> width;
+    cout << "请输入长度：";
+    cin >> length;
+    calcarea();
+}

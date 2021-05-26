6b24058857a7378032eb012ad83d8387fa60e0d5
MacKenia <maikenqi@outlook.com>
2021-05-26 12:39:40 +0800

    up

---
+++ C++/learn/2021cpp/button/som.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <windows.h>
+#include <string.h>
+#define NUM 101      //101设置原因在于字符串结束标志'\0'不能缺失
+#define SPEED 100    //控制进度增长速度
+
+void process()
+{
+	char bar[NUM];
+	memset(bar, '\0', sizeof(bar));  //初始化空间
+	const char *dynamic = "-\\|/";   //指示标志
+	for (int i = 0; i < NUM; i++)
+    {
+		printf("[%-100s][%d%%][%c]\r", bar, i, dynamic[i%4]);
+		bar[i] = '#';
+		fflush(stdout);  //强制刷新缓冲区
+		Sleep(100);    //动态假象
+	}
+	printf("\n");
+}
+
+int main() 
+{
+	system("@echo off");
+    printf("\n");
+	process();
+	return 0;
+}

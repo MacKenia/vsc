9ae6b5dd05e0968177afa8d3d57d9d017d1918df
MacKenia <maikenqi@outlook.com>
2021-05-26 13:06:51 +0800

    up

---
+++ C++/liD/t/t/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  t
+//
+//  Created by MacKenia on 2021/5/23.
+//
+
+#include <iostream>
+using namespace std;
+
+int calc()
+{
+    
+}
+
+int main() {
+    // insert code here...
+    cout << clac;
+    return 0;
+}

80db72852968d0e70d7a817cf3976b3139329baa
MacKenia <maikenqi@outlook.com>
2021-06-04 23:57:40 +0800

    up

--- C++/learn/2021cpp/222_1/222_1/main.cpp
+++ C++/learn/2021cpp/222_1/222_1/main.cpp
@@ -8,7 +8,29 @@
 #include <iostream>
 using namespace std;
 
+template <class t>
+t Max(t a, t b)
+{
+    if(a > b) return a;
+    else return b;
+}
+
+template <class t>
+t Min(t a, t b)
+{
+    if(a < b) return a;
+    else return b;
+}
+
 int main() {
     // insert code here...
+    cout << "较大的：" << endl;
+    cout << Max(1, 2) << endl;
+    cout << Max(1.0f, 2.0f) << endl;
+    cout << Max('a', 'b') << endl;
+    cout << "较小的：" << endl;
+    cout << Min('a', 'b') << endl;
+    cout << Min(1.0f, 2.0f) << endl;
+    cout << Min(1, 2) << endl;
     return 0;
 }
--- C++/learn/2021cpp/222_1/222_1/prof.h
+++
@@ -1,7 +0,0 @@
-#ifndef prof_h
-#define prof_h
-
-#include<iostream>
-
-
-#endif

fe1eb48aea58a4f35ac27d8358a67aefe068c2b4
MacKenia <maikenqi@outlook.com>
2021-06-08 16:51:33 +0800

    up

--- SQL/S-ql.sql
+++ SQL/S-ql.sql
@@ -1,5 +1,15 @@
-create schema eacherinfo
+create sch ema teacher;
+create table teacherinfo
 (
-    int(10) num PRIMARY KEY UNIQUE NOT NULL,
-    varchar(20) name 
-)
+    num int(10) PRIMARY KEY UNIQUE NOT NULL,
+    name varchar(20) not null,
+    sex varchar(4) not null,
+    birthday datetime ,
+    address varchar(50)
+);
+insert into teacherinfo values(1001,'张龙','男','1984-11-08','北京市昌平区');
+insert into teacherinfo values(1002,'李梅','女','1970-01-21','北京市海淀区');
+insert into teacherinfo values(1003,'王一丰','男','1976-10-30','北京市昌平区');
+insert into teacherinfo values(1004,'赵六','男','1980-06-05','北京市顺义区');
+delimiter .
+create procedure teachernfo1(in teacherid int, in type int, out info varchar(20))

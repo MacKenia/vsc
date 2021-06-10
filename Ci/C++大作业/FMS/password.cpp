a935238fbea444b0a3745667b878698dce34f5c5
MacKenia <maikenqi@outlook.com>
2021-06-10 19:39:53 +0800

    up

---
+++ Ci/C++大作业/FMS/password.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "           ┌──────────────────┤ VNC COMMANDS ├───────────────────┐" << endl;
+    cout << "           │                                                     │" << endl;
+    cout << "           │ You can type startvnc to start vncserver,type       │" << endl;
+    cout << "           │ stopvnc to stop it.                                 │" << endl;
+    cout << "           │ 您可以使用以下任意一条命令来启动vnc或x:             │" << endl;
+    cout << "           │ startvnc,tightvnc,tigervnc,startx11vnc,startxsdl,no │" << endl;
+    cout << "           │ vnc,输入stopvnc停止                                 │" << endl;
+    cout << "           │                                                     │" << endl;
+    cout << "           │                                                     │" << endl;
+    cout << "           │                       <确定>                        │" << endl;
+    cout << "           │                                                     │" << endl;
+    cout << "           └─────────────────────────────────────────────────────┘" << endl;
+    return 0;
+}
+
+

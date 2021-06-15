d8ac77d05e5ea3e8a153e47fc04e27257c493035
MacKenia <maikenqi@outlook.com>
2021-06-15 19:05:48 +0800

    up

--- Ci/C++FT/FMS/frd.cpp
+++ Ci/C++FT/FMS/frd.cpp
@@ -5,7 +5,7 @@ fstream frd::frdFile("friends.dat", ios::binary | ios::in | ios::out);
 
 frd::frd()
 {
-    frdFile.read((char *)fri, sizeof(fri));
+    frdFile.read((char *)&fri, sizeof(fri));
 }
 
 frd::~frd()

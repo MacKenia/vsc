de820f12c2bfa8d63876225083b0212f6d1dff03
MacKenia <maikenqi@outlook.com>
2021-06-30 14:40:32 +0800

    up

--- Ci/C++FT/FMS/main.cpp
+++ Ci/C++FT/FMS/main.cpp
@@ -28,24 +28,26 @@ int main()
     mw.get();
     mw.setW(110,32);
     frd user(lo.log());
-    int co = 500;
+    int co = 600;
     char a[] = "1.显示好友信息";
     char b[] = "2.修改好友信息";
     char c[] = "3.删除好友信息";
     char d[] = "4.添加好友";
-    char e[] = "5.退出";
+    char e[] = "5.搜索好友";
+    char f[] = "6.退出";
     user.showme(); //展示个人信息
     mw.setbc("e0"); //设置背景色
-    mw.settop(35,9); //设置左上角坐标
-    mw.bw(38,16); //输出一个界面
+    mw.settop(35,8); //设置左上角坐标
+    mw.bw(38,18); //输出一个界面
     mw.title("好友管理系统"); //更改界面标题
-    mw.ccp(2,9); //移动光标位置
-    mw.textb(41,11,"使用上下键选择，回车确认"); //输出一条信息到指定位置
-    mw.text(41,15,b); //输出作为选项的信息
-    mw.text(41,17,c);
-    mw.text(41,19,d);
-    mw.text(41,21,e);
-    mw.texta(41,13,a);
+    // mw.ccp(2,8); //移动光标位置
+    mw.textb(41,10,"使用上下键选择，回车确认"); //输出一条信息到指定位置
+    mw.text(41,14,b); //输出作为选项的信息
+    mw.text(41,16,c);
+    mw.text(41,18,d);
+    mw.text(41,20,e);
+    mw.text(41,22,f);
+    mw.texta(41,12,a);
     while(true) //选择界面
     {
         mw.get(); //获取键盘按钮
@@ -53,97 +55,132 @@ int main()
         else if(mw.button() == 2) co++;
         else if(mw.button() == 0) //回车进入
         {
-            if(co%5 == 0)
+            if(co%6 == 0)
             {//进入展示好友界面
                 user.shwf();
                 mw.setbc("e0");
-                mw.settop(35,9);
-                mw.bw(38,14);
+                mw.settop(35,8);
+                mw.bw(38,18);
                 mw.title("好友管理系统");
-                mw.textb(41,11,"使用上下键选择，回车确认");
-                mw.texta(41,13,a);
-                mw.text(41,15,b);
-                mw.text(41,17,c);
-                mw.text(41,19,d);
-                mw.text(41,21,e);
+                mw.textb(41,10,"使用上下键选择，回车确认");
+                mw.text(41,14,b);
+                mw.text(41,16,c);
+                mw.text(41,18,d);
+                mw.text(41,20,e);
+                mw.text(41,22,f);
+                mw.texta(41,12,a);
             }
-            else if(co%5 == 1)
+            else if(co%6 == 1)
             {//进入修改好友界面
                 user.modf();
                 mw.setbc("e0");
-                mw.settop(35,9);
-                mw.bw(38,14);
+                mw.settop(35,8);
+                mw.bw(38,18);
                 mw.title("好友管理系统");
-                mw.textb(41,11,"使用上下键选择，回车确认");
-                mw.texta(41,13,a);
-                mw.text(41,15,b);
-                mw.text(41,17,c);
-                mw.text(41,19,d);
+                mw.textb(41,10,"使用上下键选择，回车确认");
+                mw.text(41,12,a);
+                mw.text(41,16,c);
+                mw.text(41,18,d);
+                mw.text(41,20,e);
+                mw.text(41,22,f);
+                mw.texta(41,14,b);
             }
-            else if(co%5 == 2)
+            else if(co%6 == 2)
             {//进入删除好友界面
                 user.rmvf();
                 mw.setbc("e0");
-                mw.settop(35,9);
-                mw.bw(38,14);
+                mw.settop(35,8);
+                mw.bw(38,18);
                 mw.title("好友管理系统");
-                mw.textb(41,11,"使用上下键选择，回车确认");
-                mw.texta(41,13,a);
-                mw.text(41,15,b);
-                mw.text(41,17,c);
-                mw.text(41,19,d);
+                mw.textb(41,10,"使用上下键选择，回车确认");
+                mw.text(41,14,b);
+                mw.text(41,18,d);
+                mw.text(41,20,e);
+                mw.text(41,22,f);
+                mw.text(41,12,a);
+                mw.texta(41,16,c);
             }
-            else if(co%5 == 3)
+            else if(co%6 == 3)
             {//进入添加好友界面
                 user.addf();
                 mw.setbc("e0");
-                mw.settop(35,9);
-                mw.bw(38,14);
+                mw.settop(35,8);
+                mw.bw(38,18);
                 mw.title("好友管理系统");
-                mw.textb(41,11,"使用上下键选择，回车确认");
-                mw.texta(41,13,a);
-                mw.text(41,15,b);
-                mw.text(41,17,c);
-                mw.text(41,19,d);
+                mw.textb(41,10,"使用上下键选择，回车确认");
+                mw.text(41,14,b);
+                mw.text(41,16,c);
+                mw.text(41,20,e);
+                mw.text(41,22,f);
+                mw.text(41,12,a);
+                mw.texta(41,18,d);
+            }
+            else if(co%6 == 4)
+            {//进入搜索界面
+                user.secf();
+                mw.setbc("e0");
+                mw.settop(35,8);
+                mw.bw(38,18);
+                mw.title("好友管理系统");
+                mw.textb(41,10,"使用上下键选择，回车确认");
+                mw.text(41,14,b);
+                mw.text(41,16,c);
+                mw.text(41,18,d);
+                mw.text(41,22,f);
+                mw.text(41,12,a);
+                mw.texta(41,20,e);
             }
             else break; //退出程序
         }
-        switch(co%5)
+        switch(co%6)
         {//更新界面
             case 0:
-                mw.text(41,15,b);
-                mw.text(41,17,c);
-                mw.text(41,19,d);
-                mw.text(41,21,e);
-                mw.texta(41,13,a);
+                mw.text(41,14,b);
+                mw.text(41,16,c);
+                mw.text(41,18,d);
+                mw.text(41,20,e);
+                mw.text(41,22,f);
+                mw.texta(41,12,a);
                 break;
             case 1:
-                mw.text(41,13,a);
-                mw.text(41,17,c);
-                mw.text(41,19,d);
-                mw.text(41,21,e);
-                mw.texta(41,15,b);
+                mw.text(41,12,a);
+                mw.text(41,16,c);
+                mw.text(41,18,d);
+                mw.text(41,20,e);
+                mw.text(41,22,f);
+                mw.texta(41,14,b);
                 break;
             case 2: 
-                mw.text(41,13,a);
-                mw.text(41,15,b);
-                mw.text(41,19,d);
-                mw.text(41,21,e);
-                mw.texta(41,17,c);
+                mw.text(41,14,b);
+                mw.text(41,18,d);
+                mw.text(41,20,e);
+                mw.text(41,22,f);
+                mw.text(41,12,a);
+                mw.texta(41,16,c);
                 break;
             case 3:
-                mw.text(41,13,a);
-                mw.text(41,15,b);
-                mw.text(41,17,c);
-                mw.text(41,21,e);
-                mw.texta(41,19,d);
+                mw.text(41,14,b);
+                mw.text(41,16,c);
+                mw.text(41,20,e);
+                mw.text(41,22,f);
+                mw.text(41,12,a);
+                mw.texta(41,18,d);
                 break;
             case 4:
-                mw.text(41,13,a);
-                mw.text(41,15,b);
-                mw.text(41,17,c);
-                mw.text(41,19,d);
-                mw.texta(41,21,e);
+                mw.text(41,14,b);
+                mw.text(41,16,c);
+                mw.text(41,18,d);
+                mw.text(41,22,f);
+                mw.text(41,12,a);
+                mw.texta(41,20,e);
+                break;
+            case 5:
+                mw.text(41,14,b);
+                mw.text(41,16,c);
+                mw.text(41,18,d);
+                mw.text(41,20,e);
+                mw.text(41,12,a);
+                mw.texta(41,22,f);
                 break;
         }
     }

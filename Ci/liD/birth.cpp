15d91e0c54825996a9a3cf193abda7649ac79e28
MacKenia <maikenqi@outlook.com>
2021-08-19 20:22:36 +0800

    Update birth.cpp

--- Ci/liD/birth.cpp
+++ Ci/liD/birth.cpp
@@ -15,29 +15,34 @@ int chartoin(char *s)
 
 int main()
 {
-    fstream filein("input.txt", ios::in | ios::out | ios::app);
+    fstream filein("input.txt", ios::out | ios::in | ios::app);
     fstream fileout("output.txt", ios::out);
-    char id[13];
+    char id[20];
     int i = 1;
+    filein.close();
     cout << "请在input.txt中输入身份证，每个独占一行" << endl
     << "最终结果会输出在output.txt中" << endl
     << "完成后回车";
     getchar();
+    filein.open("input.txt",ios::in);
+    if(filein.fail())
+    {
+        cout << "初始化失败，重启程序";
+        return 0;
+    }
     while(!filein.eof())
     {
-        cout << "\r" << "正在处理第" << i << "条" << endl;
-        filein.getline(id,13,'\n');
-        if(filein.fail())
-        {
-            cout << "初始化文件失败";
-            break;
-        }
-        fileout << 2021-(chartoin(&id[6]));
+        filein.getline(id,20,'\n');
+        if(id[0] == '\0') continue;
+        cout << "正在处理第" << i++ << "条:" << id << endl;
+        // cout << id;
+        if(filein.fail()) break;
+        fileout << 2021-(chartoin(&id[6])) << endl;
         // cout << id;
     }
     fileout.close();
     filein.close();
     cout << "搞定" << endl;
     getchar();
-    return 0;
+    return 1;
 }

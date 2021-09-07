b42240a42c777be99b70f43435adff0c15592f57
Mackenia <maikenqi@outlook.com>
2021-09-07 21:30:05 +0800

    up

--- Ci/DateStructure/1.4.cpp
+++ Ci/DateStructure/1.4.cpp
@@ -9,4 +9,40 @@
  *      }
  */
 
+#include <bits/stdc++.h>
+#define N 10
+
+using namespace std;
+
+int main()
+{
+    int a[N][N], b[N][N], c[N][N];
+    int n = N;
+    for(int i = 0; i < n; i++)
+    	for(int j = 0; j < n; j++)
+	{
+		a[i][j] = rand() % 100;
+		b[i][j] = rand() % 100;
+		//cout << "a = " << a[i][j] << " b = " << b[i][j] << endl;
+	}
+    cout << "fill done" << endl;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+        {
+            // c[i][j] = 0;
+            for(int k = 0; k < n; k++)
+                c[i][j] = c[i][k] + a[i][k] * b[k][j];
+        }
+    cout << "calculate done" << endl;
+    for(int i = 0; i < n; i++)
+    {
+	    for(int j = 0; j < n; j++)
+	    {
+		    cout << c[i][j] << "\t";
+	    }
+	    cout << endl;
+    }
+    
+    return 1;
+}
 

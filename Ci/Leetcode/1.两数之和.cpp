c2089eff762b35e6f717a783ed9ef4615c202714
Mackenia <maikenqi@outlook.com>
2021-09-13 07:08:24 +0800

    up

--- Ci/Leetcode/1.两数之和.cpp
+++ Ci/Leetcode/1.两数之和.cpp
@@ -11,14 +11,18 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-
+        vector<int>::iterator it = nums.begin();
+        int i = 0;
+        for(i = 0; i < nums.size(); i++) {
+            it = find(nums.begin(), nums.end(), target-nums[i]);
+            if(i == it - nums.begin()) 
+                it = find(nums.begin() + i + 1, nums.end(), target-nums[i]);
+            if(it != nums.end()) break;
+        }
+        return {i, (int)(it - nums.begin())};
     }
 };
 
-int main()
-{
-    return 0;
-}
 
 // @lc code=end
 

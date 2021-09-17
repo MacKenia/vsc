f048dd40209c276e3e0e2fc6b022dfd4331e6bbe
mackenia <maikenqi@outlook.com>
2021-09-17 11:44:50 +0800

    up

--- Java/hello/src/main/java/com/first/Reverse.java
+++ Java/hello/src/main/java/com/first/Reverse.java
@@ -4,18 +4,26 @@ import java.util.*;
 public class Reverse {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		// int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		
 		int tmp;
-		for(int i = 0; i < arr.length/2; i++) {
-			tmp = arr[arr.length - i - 1];
-			arr[arr.length - i - 1] = arr[i];
-			arr[i] = tmp;
+		System.out.print("how many number you want to reverse:");
+		tmp = s.nextInt();
+		int[] arr = new int[tmp];
+		for(int i = 0; i < arr.length; i++){
+			arr[arr.length - i - 1] = s.nextInt();
 		}
+		//for(int i = 0; i < arr.length/2; i++) {
+		//	tmp = arr[arr.length - i - 1];
+		//	arr[arr.length - i - 1] = arr[i];
+		//	arr[i] = tmp;
+		//}
         	for (int i = 0; i < arr.length; i++) {
             		System.out.print(arr[i] + "\t");
 		}
         	System.out.print("\nPress enter to exit");
 		String str = s.nextLine();
+		str = s.nextLine();
 		s.close();
 	}
 }

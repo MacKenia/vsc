8bf21c6bf48dca990222dc96b6aba08a5462f9ef
MacKenia <maikenqi@outlook.com>
2021-09-20 16:36:47 +0800

    Update 2.1.cpp

--- Ci/DateStructure/2.1/2.1.cpp
+++ Ci/DateStructure/2.1/2.1.cpp
@@ -17,7 +17,16 @@ int main()
     Polynomial a[10];
     Polynomial b[5] = {{2,3},{1,4},{3,1},{4,5}};
     Polynomial c[5] = {{1,4},{2,5},{3,3}};
+    int m1 = 0, m2 = 0;
     for (int i = 0; i < 5; i++)
+    {
+        if(b[m1].index > b[i].index) m1 = i;
+    }
+    for(int i = 0; i < 5; i++)
+    {
+        if(c[m2].index > c[i].index) m2 = i;
+    }
+    for(int i = 0; i < 10; i++)
     {
         
     }

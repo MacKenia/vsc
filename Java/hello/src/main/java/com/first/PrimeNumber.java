c3125e70650a4f8e7b70a4f4b3e4beda68b95594
mackenia <maikenqi@outlook.com>
2021-09-24 11:25:32 +0800

    up

--- Java/hello/src/main/java/com/first/PrimeNumber.java
+++ Java/hello/src/main/java/com/first/PrimeNumber.java
@@ -4,11 +4,18 @@ import java.util.*;
 public class PrimeNumber {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		
+		boolean f = false;
 		for (int i = 2; i <= 100; i++) {
-			for (int j = i; j > 1; j--) {
-				if(i%j == 0) continue;
-			}	
+			for (int j = i - 1; j > 1; j--) {
+				f = true;
+				if(i%j == 0) {
+					f = false;
+					break;
+				}
+			}
+			if (f) {
+				System.out.println(i);
+			}
 		}
 
         	System.out.print("press enter to exit");

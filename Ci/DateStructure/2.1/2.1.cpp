8f2ca625ab8426971f4f6d11891e5c7a466f7f20
Mackenia <maikenqi@outlook.com>
2021-09-28 20:53:33 +0800

    :sob:

--- Ci/DateStructure/2.1/2.1.cpp
+++ Ci/DateStructure/2.1/2.1.cpp
@@ -1,22 +1,20 @@
 /**
  * 一元多项式的运算
- * 
  */
 
 #include <bits/stdc++.h>
+#define N 5
 
 using namespace std;
 
-struct Polynomial{
-    int coefficient;
-    int index;
-};
-
 int main()
 {
-    Polynomial a[10];
-    Polynomial b[5] = {{2,3},{1,4},{3,1},{4,5}};
-    Polynomial c[5] = {{1,4},{2,5},{3,3}};
-    int m1 = 0, m2 = 0;
+    int a[N];
+    int b[N] = {0,1,3,4};
+    int c[N] = {0,0,5,2,7};
+    for(int i = 0; i < N; i++)
+    {
+        
+    }
     return 0;
 }
--- Ci/DateStructure/2.2/2.2.cpp
+++ Ci/DateStructure/2.2/2.2.cpp
@@ -1,3 +1,25 @@
 /**
  * 稀疏多项式的运算
  */
+
+#include <bits/stdc++.h>
+#define N 4
+
+using namespace std;
+
+struct Polynomial{
+    int coefficient;
+    int exponential;
+};
+
+int main()
+{
+    Polynomial a[10];
+    Polynomial b[5] = {{2,3},{1,4},{3,1},{4,5}};
+    Polynomial c[5] = {{1,4},{2,5},{3,3}};
+    int m1 = 0, m2 = 0;
+    for(int i = 0; i < N; i++) {
+        
+    }
+    return 0;
+}

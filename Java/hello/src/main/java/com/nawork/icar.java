9d85f916d010f06bb254fe1559f4f819218d61f8
MacKenia <maikenqi@outlook.com>
2021-10-09 17:27:52 +0800

    :airplane:

---
+++ Java/hello/src/main/java/com/nawork/icar.java
@@ -0,0 +1,11 @@
+package com.nawork;
+import java.util.*;
+
+public class icar {
+    public static void main(String[] args) {
+        Car sc = new Car(1, 1, 0);
+        System.out.println("请选择操作：");
+        Scanner sc = new Scanner(System.in);
+        
+    }    
+}

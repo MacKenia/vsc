0e93373d04b93c47202b99145ba7d6a923d3c399
Mackenia <maikenqi@outlook.com>
2021-10-12 22:48:29 +0800

    :fu:

--- Ci/DateStructure/2.9/2-9.cpp
+++ Ci/DateStructure/2.9/2-9.cpp
@@ -1,19 +1,14 @@
+/**
+ * 回文数
+ */
 #include <bits/stdc++.h>
 
 using namespace std;
 
+
+
 int main()
 {
-    int i,x[10];
-    int a = 3;
-    for (int i = 0; i < a; i++)
-    {   
-        cin >> x[i];
-    }
-    for (int i = 0; i < a; i++)
-    {
-        cout << x[i];
-    }
     
     return 1;
 }

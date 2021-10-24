c0051f63f20851e6286a567eb90aa53e5d8e2e86
Mackenia <maikenqi@outlook.com>
2021-10-24 10:15:01 +0800

    :fu:

--- Java/hello/src/main/java/com/assignment1025/Animal.java
+++ Java/hello/src/main/java/com/assignment1025/Animal.java
@@ -2,5 +2,46 @@ package com.assignment1025;
 import java.util.*;
 
 public class Animal {
+    private String name;
+    private int leg;
+    private int count; 
+    public void setLeg(int a){
+        leg = a;
+    }
 
+    public int getLeg() {
+        return leg;
+    }
+
+    public void setKind(String kind){
+        name = kind;
+    }
+
+    public String getKind() {
+        return name;
+    }
+
+    public void setCount(int a) {
+        count = a;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public class Fish extends Animal {
+
+    }
+
+    public class Tiger extends Animal {
+
+    }
+    
+    public class SouthEastTiger extends Animal {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }

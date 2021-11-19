4f38c8818e3f5d5ecb6dcea3600476445d222a99
Mackenia <maikenqi@outlook.com>
2021-11-19 19:47:56 +0800

    :+1"

--- Java/hello/src/main/java/com/ass1118/Fish.java
+++ Java/hello/src/main/java/com/ass1118/Fish.java
@@ -1,22 +1,37 @@
 package com.ass1118;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
-
 public class Fish {
+
+    public static Date transform(String str) throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/mm/dd");
+        Date date = sdf.parse(str);
+        return date;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Date dateOld = new Date(0);
-        int[] date = new int[3];
-        boolean flag = false;
-        for (int i = 0; i < 3; i++) {
-            date[i] = sc.nextInt();
-        }
-        System.out.print(date);
-        int year = Integer.parseInt(String.format("%tY", dateOld)) - Integer.parseInt(String.format("%tY", date[0]));
-        int month = Integer.parseInt(String.format("%tm", dateOld)) - Integer.parseInt(String.format("%tm", date[1]));
-        int day = Integer.parseInt(String.format("%td", dateOld)) - Integer.parseInt(String.format("%td", date[2]));
+        Date date = new Date();
+        long d;
 
+        String str = sc.nextLine();
+        try{
+            date = Fish.transform(str);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        d = dateOld.getTime() - date.getTime();
+        d = (d/(365 * 24 * 60 * 60 * 1000)) % 5;
+
+        if(d < 3) {
+            System.out.println("");
+        } else {
+            System.out.println("");
+        }
         System.out.println();
         System.out.print(dateOld);
         sc.close();

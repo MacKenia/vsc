cdb83df416429a2290a0d6116bf5d2c8b05925ab
Mackenia <maikenqi@outlook.com>
2021-12-11 18:55:40 +0800

    clean

--- Ci/DateStructure/4.17/4-17.cpp
+++ Ci/DateStructure/4.17/4-17.cpp
@@ -10,7 +10,25 @@ struct tree
     tree *right;
 };
 
+void init(tree *(*node), int *arr, int n, int i = 1)
+{
+    *node = new tree;
+    (*node)->data = arr[i-1];
 
+    if(!(2*i > n))
+        init(&(*node)->left, arr, n, 2*i);
+    if(!(2*i+1 > n))
+        init(&(*node)->right, arr, n, 2*i+1);
+}
+
+int compare(tree *a, tree *b)
+{
+    if(a==NULL && b!=NULL) return 0;
+    if(a!=NULL && b==NULL) return 0;
+    if(a->data != b->data) return 0;
+    if(compare(a->left, b->left) && compare(a->right, b->right)) return 1;
+    else return 0;
+}
 
 int main()
 {

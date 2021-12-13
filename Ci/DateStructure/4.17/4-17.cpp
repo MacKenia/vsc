0f7f360512197f80ce24649b85b5e409ccac1bfe
Mackenia <maikenqi@outlook.com>
2021-12-13 22:46:35 +0800

    data

--- Ci/DateStructure/4.17/4-17.cpp
+++ Ci/DateStructure/4.17/4-17.cpp
@@ -26,12 +26,29 @@ int compare(tree *a, tree *b)
     if(a==NULL && b!=NULL) return 0;
     if(a!=NULL && b==NULL) return 0;
     if(a->data != b->data) return 0;
+    if(a == b) return 1;
     if(compare(a->left, b->left) && compare(a->right, b->right)) return 1;
     else return 0;
 }
 
 int main()
 {
-    
+    tree *a, *b;
+    int ar[N], br[N];
+    for(int i = 0; i < N; i++)
+    {
+        ar[i] = rand() % 10;
+        br[i] = rand() % 10;
+    }
+    init(&a, ar, N);
+    init(&b, br, N);
+    if(compare(a, a))
+    {
+        cout << "Two tree are the same" << endl;
+    }
+    else
+    {
+        cout << "Two tree are different" << endl;
+    }
     return 0;
 }
---
+++ Ci/DateStructure/4.18/4-18.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define N 10
+
+using namespace std;
+
+struct tree
+{
+    int data;
+    tree *left;
+    tree *right;
+};
+
+int main()
+{
+    
+    return 0;
+}

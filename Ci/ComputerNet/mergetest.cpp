52c393cd9622f896f9344b1aafffea599512670b
Mackenia <maikenqi@outlook.com>
2022-01-01 21:50:53 +0800

    chageToc++

--- Ci/ComputerNet/mergetest.cpp
+++ Ci/ComputerNet/mergetest.cpp
@@ -1,22 +1,31 @@
 #include <bits/stdc++.h>
-#include <stdio.h>
+
 #define N 5
 using namespace std;
 
 int main()
 {
-    FILE *output, *input;
+    // FILE *output, *input;
+    fstream Input, Output;
     char tmpname[] = "t0";
     char buf[1024];
-    output = fopen("merge.mp4","wb+");
+    // output = fopen("merge.mp4","wb+");
+    Output.open("merge.mp4",ios::binary | ios::out);
     for (int i = 0; i < N; i++)
     {
         int size = 0, Ksize = 0, Rsize = 0;
         tmpname[1]++;
-        input = fopen(tmpname, "rb");
-        fseek(input, 0, SEEK_END);
-        size = ftell(input);
-        fseek(input, 0, SEEK_SET);
+
+        // input = fopen(tmpname, "rb");
+        // fseek(input, 0, SEEK_END);
+        // size = ftell(input);
+        // fseek(input, 0, SEEK_SET);
+
+        Input.open(tmpname,ios::in | ios::binary);
+        Input.seekg(0,ios::end);
+        size = Input.tellg();
+        Input.seekg(0);
+
         cout << tmpname << ":" << size << endl;
         Ksize = size%1024 ? size/1024 + 1 : size/1024;
         Rsize = 1024;
@@ -24,12 +33,16 @@ int main()
         {
             if(i == N - 1 && j == Ksize - 1) Rsize = size%1024;
             memset(buf, 0, sizeof(buf));
-            fread(&buf,Rsize,1,input);
-            fwrite(&buf,Rsize,1,output);
+
+            // fread(&buf,Rsize,1,input);
+            // fwrite(&buf,Rsize,1,output);
+            Input.read(buf,Rsize);
+            Output.write(buf,Rsize);
         }
-        fclose(input);
+        // fclose(input);
+        Input.close();
     }
-    fclose(output);
-    
+    // fclose(output);
+    Input.close();
     return 0;
 }

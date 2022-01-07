53070ff0de7256b9703d68e2999dbff768ccbccd
MacKenia <maikenqi@outlook.com>
2022-01-07 23:16:44 +0800

    calculator

--- Java/main/src/main/java/com/calculator/Calculator.java
+++ Java/main/src/main/java/com/calculator/Calculator.java
@@ -25,29 +25,29 @@ import javax.swing.plaf.DimensionUIResource;
 
 public class Calculator extends JFrame {
     public Container ct = getContentPane();
-
+    // 布局管理器
     final FlowLayout fl = new FlowLayout();
     public JSplitPane sp = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, true);
     private BorderLayout bl = new BorderLayout();
     private GridLayout gl = new GridLayout(4,5);
-
+    // 输入框
     private JTextField in1 = new JTextField();
     private JTextField in2 = new JTextField();
     private JTextField op = new JTextField();
     private JTextField out = new JTextField();
     private JTextArea history = new JTextArea();
-    
+    // 按钮集合 利于调用
     private JTextField[] text = {in1, op, in2, out};
     private JButton[] but = new JButton[23];
     private JPanel[] pan = new JPanel[5];
     private int pans = 0;
     private int buts = 0;
-    
+    // 个性化设置
     private Insets is = new Insets(2,2,2,2);
     private Font ft = new Font("Arial",Font.BOLD,17);
-    
+    // 监听
     private Control listener = new Control(but, text, history);
-    
+    // 构造函数
     Calculator() {
         super();
         setTitle("计算器");
@@ -56,24 +56,25 @@ public class Calculator extends JFrame {
         setContentPane(sp);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
-        
+        // 装入左右面板
         leftPanel();
         rightPanel();
-        
+        // 对按钮进行修改
         for(JButton i : but){
             i.setPreferredSize(new Dimension(52,32));
         }
-        
+        // 设置split属性
         sp.setContinuousLayout(true);
         sp.setDividerLocation(JSplitPane.CENTER_ALIGNMENT);
         sp.setDividerSize(0);
-
+        // 更换字体
         for(JTextField i : text){
             i.setHorizontalAlignment(JTextField.CENTER);
             i.setMargin(is);
             i.setFont(ft);
             i.setEditable(false);
         }
+        // 更换主题
         try{
             for(UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
                 if("Nimbus".equals(info.getName())) {
@@ -83,17 +84,17 @@ public class Calculator extends JFrame {
         } catch(Exception e) {
             e.printStackTrace();
         }
-        
+        // 重新设置可见
         setVisible(true);
     }
-    
+    // 左面板
     void leftPanel() {
         JPanel leftPanel = new JPanel(fl);
         leftPanel.add(inputField());
         leftPanel.add(buttons());
         sp.setLeftComponent(leftPanel);
     }
-
+    // 右面板
     void rightPanel() {
         JPanel rigJPanel = new JPanel(fl);
         rigJPanel.add(outputField());
@@ -101,7 +102,7 @@ public class Calculator extends JFrame {
         rigJPanel.add(extraFunction());
         sp.setRightComponent(rigJPanel);
     }
-
+    // 添加按钮
     JPanel buttons () {
         int BPan = pans++;
         pan[BPan] = new JPanel(gl);
@@ -120,7 +121,7 @@ public class Calculator extends JFrame {
         }
         return pan[BPan];
     }
-
+    // 添加输入框
     JPanel inputField(){
         int FPan = pans++;
         pan[FPan] = new JPanel(fl);
@@ -134,7 +135,7 @@ public class Calculator extends JFrame {
         pan[FPan].add(in2);
         return pan[FPan];
     }
-
+    // 添加输出框
     JPanel outputField(){
         int FPan = pans++;
         pan[FPan] = new JPanel(fl);
@@ -142,7 +143,7 @@ public class Calculator extends JFrame {
         pan[FPan].add(out);
         return pan[FPan];
     }
-
+    // 添加历史记录框
     JPanel historyArea(){
         int APan = pans++;
         JScrollPane scp = new JScrollPane();
@@ -157,7 +158,7 @@ public class Calculator extends JFrame {
         pan[APan].add(scp);
         return pan[APan];
     }
-
+    // 添加额外功能键
     JPanel extraFunction(){
         int EPan = pans++;
         int first = buts;
@@ -172,7 +173,7 @@ public class Calculator extends JFrame {
             but[buts].setMargin(new Insets(0,0,0,0));
             pan[EPan].add(but[buts++]);
         }
-        
+        // 监听
         class cate implements ActionListener{
             public void actionPerformed(ActionEvent e){
                 if((((JButton)e.getSource()).getActionCommand()).equals("保存")){

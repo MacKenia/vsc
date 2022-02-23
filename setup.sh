dadac9861713da3c59fc0834651f6033ef939ea5
Mackenia <maikenqi@outlook.com>
2022-02-23 13:35:52 +0800

    set

--- setup.sh
+++ setup.sh
@@ -1,21 +1,72 @@
-# /usr/bin/bash
+# /bin/bash
+# my own archlinux setup bash
+if [ ! $1 ]
+then
+    echo "Please enter a username!(sh setup.sh [username])"
+    exit
+fi
 
-arch-chroot
+pacman -Sy --needed --noconfirm neovim ranger zsh openssh
 
-ls -s /usr/share/zoneinfo/Asia/Shanghai > /etc/localtime
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+timedatectl set-ntp true
+
 hwclock --systohc
-echo "en_US.UTF-8 UTF-8" > /etc/local.gen
-echo "zh_CN.UTF-8 UTF-8" > /etc/local.gen
-local-gen
-echo "LANG=en_US.UTF-8" > /etc/local.conf
-export LANG=en_US.UTF-8
 
-echo "Mac-Arch" > /etc/hostname
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "zh_CN.UTF-8 UTF-8" >> /etc/locale.gen
 
+locale-gen
 
-pacman -S 
-adduser -m -G wheel,power,storage -s /bin/bash mackenia
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
+echo "$1" > /etc/hostname
 
-bash -c "$(curl -Lv gitee.com/mo2/liunx/raw/2/2)"
+echo "127.0.0.1        localhost" >> /etc/hosts
+echo "::1               localhost" >> /etc/hosts
+echo "127.0.1.1        $1" >> /etc/hosts
 
+echo "Please enter passwd for root:\n"
+
+passwd root
+
+pacman -S --needed --noconfirm intel-ucode grub efibootmgr
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi
+
+sed -i 's/loglevel=3 quiet/loglevel=5 nowatchdog/g' /etc/default/grub
+echo "GRUB_DISABLE_OS_PROBER=FALSE" >> /etc/default/grub
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+useradd -m -G wheel -s /bin/zsh $1
+
+echo "Please enter passwd for $1\n"
+
+passwd $1
+
+systemctl enable dhcpcd
+
+echo "%wheel ALL=(ALL:ALL) ALL\n\
+Defaults rootpw" >> /etc/sudoers
+
+pacman -S --needed --noconfirm ntfs-3g adobe-source-han-serif-cn-fonts wqy-zenhei noto-fonts-cjk noto-fonts-emoji noto-fonts-extra fcitx5-im fcitx5-chinese-addons
+
+echo "GTK_IM_MODULE=fcitx\n\
+QT_IM_MODULE=fcitx\n\
+XMODIFIERS=@im=fcitx\n\
+SDL_IM_MODULE=fcitx" >> /etc/environment
+
+echo "[multilib]\n\
+Include = /etc/pacman.d/mirrorlist\n\
+[archlinuxcn]\n\
+Server = https://mirrors.ustc.edu.cn/archlinuxcn/\$arch" >> /etc/pacman.conf
+
+pacman -Sy --needed --noconfirm archlinuxcn-keyring
+pacman -S --needed --noconfirm yay
+yay -Sy --needed --noconfirm nerd-fonts-source-code-pro microsoft-edge-dev-bin
+
+pacman -S --needed --noconfirm htop 
+
+# bash -c "$(curl -Ssfv https://gitee.com/mo2/zsh/raw/master/zsh.sh)"

7ec753f63d031ec8ea6505c9ea420737a9e375c2
Mackenia <maikenqi@outlook.com>
2022-03-08 21:01:13 +0800

    :pencil2:

--- Ci/Algorithm/SortArr.c
+++ Ci/Algorithm/SortArr.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-void sort(int, int*);
+void InsertSort(int, int*);
+void BubbleSort(int, int*);
 
 int main()
 {
@@ -10,16 +11,31 @@ int main()
     {
         scanf("%d", &arr[i]);
     }
-    sort(N, arr);
+    BubbleSort(N, arr);
     for (int i = 0; i < N; i++)
     {
         printf("%d, ", arr[i]);
     }
-    
     return 1;
 }
 
-void sort(int N, int *p)
+void BubbleSort(int N, int *p)
+{
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N - i ; j++)
+        {
+            if(p[j] >= p[j+1])
+            {
+                int t = p[j];
+                p[j] = p[j+1];
+                p[j+1] = t;
+            }
+        }
+    }
+}
+
+void InsertSort(int N, int *p)
 {
     for (int i = 1; i < N; i++)
     {

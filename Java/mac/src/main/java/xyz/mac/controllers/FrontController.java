14ceb71042bfed33d4c38d3e454b4fa6880fd66e
Mackenia <maikenqi@outlook.com>
2022-04-14 13:46:28 +0800

    springboot pa

--- Java/mac/src/main/java/xyz/mac/controllers/FrontController.java
+++ Java/mac/src/main/java/xyz/mac/controllers/FrontController.java
@@ -2,10 +2,16 @@ package xyz.mac.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import xyz.mac.model.Student;
 import xyz.mac.model.Teacher;
@@ -20,16 +26,17 @@ public class FrontController {
     @Autowired
     TeacherServices teacherServices;
 
-    @GetMapping("/Student")
-    public String stu(Model model){
+    @PostMapping("/Student")
+    public String stu(Model model,@RequestParam("stuName") String name, int age){
         List<Student> stuList = studentServices.getAllStudents();
         System.out.println(stuList);
         model.addAttribute("stuList",stuList);
+        model.addAttribute("info",name+" "+age);
         return "Student";
     }
     
-    @GetMapping("/Teacher")
-    public String tea(Model model) {
+    @PostMapping("/Teacher")
+    public String tea(Model model, Teacher tea) {
         List<Teacher> teaList = teacherServices.getAllTeachers();
         System.out.println(teaList);
         model.addAttribute("teaList",teaList);
@@ -37,12 +44,30 @@ public class FrontController {
     }
 
     @GetMapping("/commonToIndex")
-    public String index(Model model) {
+    public String index(Model model, String name, int age) {
         return "index";
     }
 
     @GetMapping("/layoutToMain")
     public String main(Model model) {
         return "main";
+    }
+
+    @GetMapping("index")
+    public String infoO(Model model) {
+        return "index";
+    }
+
+    @PostMapping("info")
+    public String infoS(HttpServletRequest request) {
+        request.getParameter("name");
+        request.getRemoteAddr();
+        Cookie[] cookies = request.getCookies();
+        return "info";
+    }
+
+    @GetMapping("info/{name}/{age}")
+    public String infoT(@PathVariable String name, @PathVariable int age) {
+        return "info";
     }
 }

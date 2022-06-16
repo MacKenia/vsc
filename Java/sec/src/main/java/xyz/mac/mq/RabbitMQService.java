7398ea9bc8deefd5dceff9e11f50e19c618dd2c8
Mackenia <maikenqi@outlook.com>
2022-06-16 09:58:31 +0800

    Java Mq

--- Java/sec/src/main/java/xyz/mac/mq/RabbitMQService.java
+++ Java/sec/src/main/java/xyz/mac/mq/RabbitMQService.java
@@ -1,6 +1,9 @@
 package xyz.mac.mq;
 
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -19,4 +22,26 @@ public class RabbitMQService {
         String s = new String(body);
         System.out.println("短信业务接收到消息： " + s);
     }
+
+    @RabbitListener(bindings = @QueueBinding(value = @Queue("routing_queue_error"), exchange = @Exchange(value = "exchange_why", type = "direct"), key = "error_routing_key"))
+    public void routingConsumerError(String message) {
+        System.out.println("接收到error级别日志消息: " + message);
+    }
+
+    @RabbitListener(bindings = @QueueBinding(value = @Queue("routing_queue_all"), exchange = @Exchange(value = "exchange_why", type = "direct"), key = {
+            "error_routing_key", "info_routing_key", "warning_routing_key" }))
+    public void routingConsumerAll(String message) {
+        System.out.println("接收到info、error、warning等级别日志消息: " + message);
+    }
+
+    @RabbitListener(bindings = @QueueBinding(value = @Queue("topic_queue_email"), exchange = @Exchange(value = "exchange_when", type = "topic"), key = "info.#.email.#"))
+    public void topicConsumerEmail(String message) {
+        System.out.println("接收到邮件订阅需求处理消息： " + message);
+    }
+
+    @RabbitListener(bindings = @QueueBinding(value = @Queue("topic_queue_sms"), exchange = @Exchange(value = "exchange_when", type = "topic"), key = "info.#.sms.#"))
+    public void topicConsumerSms(String message) {
+        System.out.println("接收到短信订阅需求处理消息： " + message);
+    }
+
 }
--- Java/sec/src/test/java/xyz/mac/sec/SecApplicationTests.java
+++ Java/sec/src/test/java/xyz/mac/sec/SecApplicationTests.java
@@ -1,16 +1,19 @@
 package xyz.mac.sec;
 
 import java.util.Date;
-import xyz.mac.model.User;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import xyz.mac.model.User;
 
 @SpringBootTest
 class SecApplicationTests {
@@ -33,13 +36,13 @@ class SecApplicationTests {
 
 	@Test
 	public void amqpAdmin() {
-		amqpAdmin.declareExchange(new FanoutExchange("fanout_exchange_one"));
+		amqpAdmin.declareExchange(new FanoutExchange("exchange_what"));
 		amqpAdmin.declareQueue(new Queue("fanout_queue_email"));
 		amqpAdmin.declareQueue(new Queue("fanout_queue_sms"));
 		amqpAdmin.declareBinding(new Binding("fanout_queue_email",
-				Binding.DestinationType.QUEUE, "fanout_exchange", "", null));
+				Binding.DestinationType.QUEUE, "exchange_what", "", null));
 		amqpAdmin.declareBinding(new Binding("fanout_queue_sms",
-				Binding.DestinationType.QUEUE, "fanout_exchange", "", null));
+				Binding.DestinationType.QUEUE, "exchange_what", "", null));
 	}
 
 	@Test
@@ -47,15 +50,52 @@ class SecApplicationTests {
 		User user = new User();
 		user.setId(1);
 		user.setUsername("科比");
-		rabbitTemplate.convertAndSend("fanout_exchange_one", "", user);
+		rabbitTemplate.convertAndSend("exchange_what", "", user);
 
 		user.setId(2);
 		user.setUsername("谷爱凌");
-		rabbitTemplate.convertAndSend("fanout_exchange_one", "", user);
+		rabbitTemplate.convertAndSend("exchange_what", "", user);
 
 		user.setId(3);
 		user.setUsername("丁真");
-		rabbitTemplate.convertAndSend("fanout_exchange_one", "", user);
+		rabbitTemplate.convertAndSend("exchange_what", "", user);
+	}
+
+	@Test
+	public void routingCreate() {
+		amqpAdmin.declareExchange(new DirectExchange("exchange_why"));
+		amqpAdmin.declareQueue(new Queue("routing_queue_error"));
+		amqpAdmin.declareBinding(new Binding("routing_queue_error", Binding.DestinationType.QUEUE, "exchange_why",
+				"error_routing_key", null));
+	}
+
+	@Test
+	public void routingPublisher() {
+		rabbitTemplate.convertAndSend("exchange_why",
+				"error_routing_key",
+				"routing send  error message");
+	}
+
+	@Test
+	public void topicCreate() {
+		amqpAdmin.declareExchange(new TopicExchange("exchange_when"));
+		amqpAdmin.declareQueue(new Queue("topic_queue_email"));
+		amqpAdmin.declareQueue(new Queue("topic_queue_sms"));
+		amqpAdmin.declareBinding(new Binding("topic_queue_email", Binding.DestinationType.QUEUE, "exchange_when",
+				"topic.email.#", null));
+		amqpAdmin.declareBinding(new Binding("topic_queue_sms", Binding.DestinationType.QUEUE, "exchange_when",
+				"topic.sms.#", null));
+	}
+
+	@Test
+	public void topicPublisher() {
+		rabbitTemplate.convertAndSend("exchange_when", "info.email",
+				"topics send  email message");
+		rabbitTemplate.convertAndSend("exchange_when", "info.sms",
+				"topics send  sms message");
+		rabbitTemplate.convertAndSend("exchange_when",
+				"info.email.sms",
+				"topics send  email and sms message");
 	}
 
 }
